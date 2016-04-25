c96ea04d0671e2a0c5305e767249156d84554346
granuloeldar <granulo.eldar@gmail.com>
2016-04-25 13:16:02 +0200

    Uklanjanje .js fileova i test ruta

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,11 @@ app.use(express.static('./client'));
 app.use(express.static('./client/css'));
 app.use(express.static('./client/js'));
 
+app.get('/api/hello', function (request, response) { 
+    var dummy: string = '{"text": "dummy text"}';
+    response.send(JSON.parse(dummy));
+});
+
 app.listen(3000, function () {
     console.log('Server started at localhost:3000');
 });
